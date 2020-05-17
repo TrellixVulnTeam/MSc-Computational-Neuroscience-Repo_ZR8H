d86abd9614322b80f2f26dcd1b203c10d26c1e33
Eran Shorer <eshorer96@gmail.com>
2020-05-17 17:34:38 +0200

    Figure 1D working
    Not exactly the same as Kira's as she uses different starting concentrations for K+ and X

--- Fig1_Single_Compartment_PLM.py
+++ Fig1_Single_Compartment_PLM.py
@@ -7,10 +7,10 @@ Created on Fri May  8 10:28:24 2020
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-from Graphing import clcolor, kcolor, xcolor,nacolor,wcolor
+from Graphing import clcolor, kcolor, xcolor,nacolor
 import Class_Single_Compartment_PLM as PLM
 
-#### Figure 1B
+#### Figure 1
 
 def F1b(init_cl=[1e-3,15e-3,50e-3,90e-3]):
     
@@ -39,7 +39,6 @@ def F1b(init_cl=[1e-3,15e-3,50e-3,90e-3]):
             Subplot_2.plot(Fig1B_Sim.t_arr,Fig1B_Sim.volume_arr,color='k',ls='--')
        
 def F1c():
-    
     
       
     Fig1C_Sim = PLM.SingleCompartment_PumpLeak()
@@ -72,10 +71,54 @@ def F1c():
     ax3.legend()
     sns.despine()   
     
-    Fig1
-    
 
-F1c()
+def F1d(rates=[-7,-3,-1,1]):
+        
+    na_arr = []
+    k_arr =[]
+    cl_arr =[]
+    x_arr =[]
+    vm_arr =[]
+    w_arr =[]
+    
+    
+    for i in range(len(rates)):       
+                      
+        Fig1D_Sim = PLM.SingleCompartment_PumpLeak()
+        Fig1D_Sim.Set_Concentrations()
+        Fig1D_Sim.Set_LeakConductances()
+        Fig1D_Sim.Set_NaKATPase_Properties(ATP_pump_rate = rates[i],time_off=1000000,time_on=0,switch_at_start='on')
+        Fig1D_Sim.Set_KCC2_Properties()
+        Fig1D_Sim.Set_Timing(duration = 1200 ,time_step = 1e-3)
+        Fig1D_Sim.Simulate_PLM()
+        na_arr.append(Fig1D_Sim.na_arr[-1]) 
+        k_arr.append(Fig1D_Sim.k_arr[-1]) 
+        cl_arr.append(Fig1D_Sim.cl_arr[-1]) 
+        x_arr.append(Fig1D_Sim.x_arr[-1]) 
+        vm_arr.append(Fig1D_Sim.vm_arr[-1]) 
+        w_arr.append(Fig1D_Sim.volume_arr[-1]) 
+        
+    Figure_1D, (ax1,ax2,ax3) = plt.subplots(nrows=3, ncols=1, sharex='all')
+        
+    ax1.plot(rates,cl_arr,color=clcolor,ls='solid',label='[Cl]')
+    ax1.plot(rates,k_arr,color=kcolor,ls='solid', label='[K]')
+    ax1.plot(rates,na_arr,color=nacolor,ls='solid', label = '[Na]')
+    ax1.plot(rates,x_arr,color=xcolor,ls='solid', label ='[X]')
+    ax1.set_ylabel('Concentrations(mM)')
+    #ax1.legend()
+        
+    ax2.plot(rates,vm_arr,color='k',ls='solid', label='Vm')
+    ax2.set_ylabel('Voltages (mV)')
+    #ax2.legend()
+    
+    ax3.plot(rates,w_arr,color='k',ls='solid', label='Volume')
+    ax3.set_ylabel('Volume (pL)')
+    ax3.set_xlabel('NA/K ATPase pump rate constant')
+    #ax3.legend()
+    sns.despine()   
+
 
 
 #F1b()
+#F1c()
+#F1d()

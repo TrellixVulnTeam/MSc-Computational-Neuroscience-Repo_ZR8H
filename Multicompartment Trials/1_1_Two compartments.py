a646bb7885875fba102e96f1159fc2ffd7234781
Eran Shorer <eshorer96@gmail.com>
2020-10-13 15:01:59 +0200

    Small update

--- Multicompartment Trials/1_1_Two compartments.py
+++ Multicompartment Trials/1_1_Two compartments.py
@@ -3,5 +3,22 @@
 Created on Tue Oct 13 14:46:28 2020
 
 @author: eshor
-"""
 
+This trial is simply to see if I can model diffusion and drift between two compartments
+Each compartment will have the same pump and leak mechanisms as in the single compartment
+
+"""
+import numpy as np
+from constants import F
+from common import default_radius, default_length, \
+    clo, ko, nao, xo_z, oso, \
+    default_p, \
+    gk, gna, gcl, \
+    ck, cna, \
+    pw, vw, km, \
+    RTF, RT
+
+class Compartment():
+    
+    def __init__(self,)
+    

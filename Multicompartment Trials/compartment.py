f751a7de0fdb850a864d7353d85ddb28584b6b86
Eran Shorer <eshorer96@gmail.com>
2021-03-12 10:09:56 +0200

    Code neatening and minor updates

--- Multicompartment Trials/compartment.py
+++ Multicompartment Trials/compartment.py
@@ -4,91 +4,105 @@ Created on Sat Jan  2 17:45:32 2021
 
 @author: eshor
 
+Class which defines the compartments object and related methods.
+
+Class:
+    Compartment : New compartment
+Methods:
+    __int__ : Initializes compartment object
+    set_ion_properties: define intracellular ionic properties of the compartment (extracellular properties are imported)
+    step: actions to take at every time point in the simulation
+    update_volumes: change the volume of the compartment (as well as ion concentrations based on new volume)
+    update_arrays: update the arrays for each parameter of the compartment
+    ed_update: make changes to the compartment based on the results of electrodiffusion
+    get_ed_dict: sends the current status of the compartment back to the simulation to be evaluated by electrodiffusion equations
+    get_fin_vals: sends the final values of the compartment back to the simulation
+    get_df_array: sends the dataframe arrays back to the simulation
+
 
 
 """
-import numpy as np
-import pandas as pd
 
-# import copy -- WTF is that
-from common import default_radius, default_length, default_p, default_Cm, \
-    oso, nao, ko, clo, \
+##################################################################################
+# IMPORTS
+
+import numpy as np
+
+
+from common import  oso, nao, ko, clo, \
     gk, gna, gcl, \
     pw, vw, RTF
 from constants import F
 
+##################################################################################
+# COMPARTMENT CLASS
 
-class Compartment():
+class Compartment:
 
 
-    def __init__(self, compartment_name, radius=5e-5, length=10e-5, Cm=2e-4, pkcc2=2e-3 / F,
+    def __init__(self, compartment_name, radius=5e-5, length=10e-5, cm=2e-4, pkcc2=2e-3 / F,
                  p=1):
+        """
+        Initializing the compartment object:
+        Establish:
+        - Compartment name
+        - Dimensions
+        - Pump rates
+        Defines all relevant variables
+        """
         self.name = compartment_name
         self.radius = radius  # in dm
         self.length = length  # in dm
         self.vw = vw
         self.pw = pw
         self.w = np.pi * (self.radius ** 2) * self.length
+        self.dw= 0
+        self.w2 =0
         self.w_temp = self.w
         self.sa = 2 * np.pi * self.radius * self.length
         self.ar = self.sa/self.w
-        self.C = Cm
+        self.C = cm
         self.FinvCAr = F / (self.C * self.ar)
         self.p_kcc2 = pkcc2
+        self.j_kcc2 = 0
         self.p = (10**p)/F
+        self.j_p = 0
         self.v = 0
         self.E_cl = 0
         self.E_k = 0
+
+
         self.na_i = 0
         self.k_i = 0
         self.cl_i = 0
         self.x_i = 0
         self.z_i = 0
+        self.na_o = 0
+        self.k_o = 0
+        self.cl_o = 0
+        self.osm_i= 0
+        self.osm_o = 0
 
-    def set_ion_properties(self, na_i=14.002e-3, k_i=122.873e-3, cl_i=5.163e-3, x_i=154.962e-3, z_i=-0.85, g_x=0e-9):
+        self.na_ramp =0
+        self.diff = 0
 
-        # A) Intracellular ion properties:
-        self.na_i = na_i
-        self.k_i = k_i
-        self.cl_i = cl_i
-        self.z_i = z_i
-        self.x_i = x_i
-
-        """Impermeant anion/Sodium Ramp to ensure electroneutrality at the beginning of simulations"""
-        if self.x_i*self.z_i<=-140e-3: #-140 is a value chosen for when the ramp should be activated based on when system was crashing based on too much impermeants.
-            self.diff = (self.x_i*self.z_i)+140e-3
-            self.na_ramp = self.diff*-1  #Adding some extra intracellular sodium to the compartment to offset the change of impermeants
-            self.na_i += self.na_ramp
-
-
-        """if cl_i == 0:
-            # setting chloride that is osmo- and electro-neutral initially.
-            self.cl_i = (oso + (self.na_i + self.k_i) * (1 / self.z_i - 1)) / (1 + self.z_i)
-      
-      if self.k_i == 0:
-            self.x_i = 155.858e-3
-            self.k_i = self.cl_i-self.z_i*self.x_i-self.na_i
-      else:
-            self.x_i = (self.cl_i - self.k_i - self.na_i) / self.z_i """
-
-        self.g_x = g_x/F  # basically 0 ... therefore impermeant
-        self.g_na = gna
-        self.g_k = gk
-        self.g_cl = gcl
-
-        # C) Extracellular ion properties:
-        self.na_o = nao
-        self.k_o = ko
-        self.cl_o = clo
-        self.osm_o = oso
-
-        # D) Zeroing Delta values
+        # Zeroing Delta values
         self.d_na_i = 0
         self.d_k_i = 0
         self.d_cl_i = 0
         self.d_x_i = 0
 
-        # F) Zeroing arrays
+        #Zeroing conductances
+        self.g_x = 0  # basically 0 ... therefore impermeant
+        self.g_na = 0
+        self.g_k = 0
+        self.g_cl = 0
+
+        self.dt =0
+        self.syn_t_off =0
+        self.syn_t_on =0
+
+        # Zeroing arrays
         self.na_arr = []
         self.k_arr = []
         self.cl_arr = []
@@ -105,28 +119,74 @@ class Compartment():
         self.osm_i_arr = []
         self.osm_o_arr = []
 
+
+
+    def set_ion_properties(self, na_i=14.002e-3, k_i=122.873e-3, cl_i=5.163e-3, x_i=154.962e-3, z_i=-0.85, g_x=0e-9):
+        """
+        - Internal ionic concentrations and leak conductance.
+        - Adjustment of starting concentrations to ensure starting electroneutrality
+        - External ionic concentrations (based on imports)
+        - Ionic conductances (based on imports)
+        """
+        # Intracellular ion properties:
+
+        self.na_i = na_i
+        self.k_i = k_i
+        self.cl_i = cl_i
+        self.z_i = z_i
+        self.x_i = x_i
+
+        # Impermeable anion/Sodium adjustment to ensure electroneutrality at the beginning of simulations
+        if self.x_i*self.z_i<=-140e-3: #-140 is a value chosen for when the ramp should be activated based on when system was crashing based on too much impermeants.
+            self.diff = (self.x_i*self.z_i)+140e-3
+            self.na_ramp = self.diff*-1  #Adding some extra intracellular sodium to the compartment to offset the change of impermeants
+            self.na_i += self.na_ramp
+
+
+        """if cl_i == 0:
+            # setting chloride that is osmo- and electro-neutral initially.
+            self.cl_i = (oso + (self.na_i + self.k_i) * (1 / self.z_i - 1)) / (1 + self.z_i)
+      
+           if self.k_i == 0:
+            self.x_i = 155.858e-3
+            self.k_i = self.cl_i-self.z_i*self.x_i-self.na_i
+           else:
+            self.x_i = (self.cl_i - self.k_i - self.na_i) / self.z_i """
+
+
+
+        # Extracellular ion properties:
+        self.na_o = nao
+        self.k_o = ko
+        self.cl_o = clo
+        self.osm_o = oso
+
+        #Ionic conductance
+        self.g_x = g_x / F  # basically 0 ... therefore impermeant
+        self.g_na = gna
+        self.g_k = gk
+        self.g_cl = gcl
+
+
+
     def step(self, dt=1e-3):
         """
         Perform a time step for the specific compartment.
-        
-        1)	Calculate the voltages in each compartment
-        2)	Calculate the KCC2 and ATPase pump rate in each compartment (different)
-        3)	Solve the ion flux in the Y direction across the membrane for each compartment (electrochemical gradient + pumps)
-        4)	Solve the ion flux for each ion in the x direction (electrodiffusion equation)
-        5)	Make adjustments for the ion concentrations based on movement between compartments
-        6)	Calculate the volume change in each compartment
-        7)	Correct ion concentrations based on new volumes
+        1)  Reset deltas to zero
+        2)	Calculate the voltages in each compartment
+        3)	Calculate the KCC2 and ATPase pump rate in each compartment (different)
+        4)	Solve the ion flux in the Y direction across the membrane for each compartment (electrochemical gradient + pumps)
+        5)  Update ion concentrations
 
-        
         """
-
+        #1) Zeroing deltas
         self.dt = dt
         self.d_na_i = 0
         self.d_k_i = 0
         self.d_cl_i = 0
         self.d_x_i = 0
 
-        # 1) Updating voltages
+        # 2) Updating voltages
         self.v = self.FinvCAr * (self.na_i + self.k_i + (self.z_i * self.x_i) - self.cl_i)
 
         if self.cl_i < 0:
@@ -137,11 +197,11 @@ class Compartment():
         self.E_k = -1 * RTF * np.log(self.k_i / self.k_o)
         self.E_cl = RTF * np.log(self.cl_i / self.cl_o)
 
-        # 2) Update ATPase and KCC2 pump rate
+        # 3) Update ATPase and KCC2 pump rate
         self.j_p = self.p * (self.na_i / nao) ** 3
         self.j_kcc2 = self.p_kcc2 * (self.E_k - self.E_cl)
 
-        # 3) Solve ion flux equations for t+dt from t
+        # 4) Solve ion flux equations for t+dt from t
 
         self.d_na_i = - self.dt * self.ar * (
                 self.g_na * (self.v + RTF * np.log(self.na_i / self.na_o)) + 3 * self.j_p)
@@ -150,22 +210,18 @@ class Compartment():
                 self.g_k * (self.v + RTF * np.log(self.k_i / self.k_o)) - 2 * self.j_p - self.j_kcc2)
 
         self.d_cl_i = + self.dt * self.ar * (self.g_cl * (self.v + RTF * np.log(self.cl_o / self.cl_i)) + self.j_kcc2)
+
+        # 5) Update ion concentrations
         self.na_i = self.na_i + self.d_na_i
         self.k_i = self.k_i + self.d_k_i
         self.cl_i = self.cl_i + self.d_cl_i
 
-        # 4) Electrodiffusion calculations
-
-        # 6) Test Arrays:
 
     def update_volumes(self):
-        # 6) Update volume
-        ''' Calculates the new compartment volume (dm3)
-        Elongation should occur length ways not radially
-        '''
+        """ Calculates the new compartment volume (dm3)
+        Elongation should occur radially
+        """
         self.osm_i = self.na_i + self.k_i + self.cl_i + self.x_i
-
-
 
         self.dw = self.dt * (self.vw * self.pw * self.sa * (self.osm_i - self.osm_o))
         self.w2 = self.w + self.dw
@@ -177,16 +233,17 @@ class Compartment():
 
         self.w = self.w2
 
-        # self.length = self.w / (np.pi * self.radius ** 2)
         self.radius = np.sqrt(self.w / (np.pi * self.length))
-        self.sa = 2 * (np.pi) * (self.radius) * (self.length)
+        self.sa = 2 * np.pi * self.radius * self.length
         self.ar = self.sa / self.w
         self.FinvCAr = F / (self.C * self.ar)
 
-        ##### Radius and length will also have to be updated for electrodiffusion calculation
+
 
     def update_arrays(self):
-
+        """
+        Update arrays such that they reflect the actual values
+        """
         self.na_arr.append(self.na_i * 1000)
         self.k_arr.append(self.k_i * 1000)
         self.cl_arr.append(self.cl_i * 1000)
@@ -204,6 +261,9 @@ class Compartment():
         self.osm_o_arr.append(self.osm_o*1000)
 
     def ed_update(self, ed_change: dict, sign="positive"):
+        """
+        Receives a dictionary and update
+        """
         if sign == "positive":
             self.na_i += ed_change["na"]
             self.cl_i += ed_change["cl"]
@@ -235,3 +295,24 @@ class Compartment():
         df_arr = [self.radius, self.length, self.w, self.na_i, self.k_i, self.cl_i, self.x_i, self.z_i, self.p,
                   self.p_kcc2, self.v, self.E_k, self.E_cl, ]
         return df_arr
+
+    def add_synapse(self,syn_t_on= 0, syn_t_off = 0, Inhibitory_synapses = 0, Excitatory_synapses = 0, Ramp =True):
+        """
+        Set time synapse is turned on and off
+        Inhibitory_synapses = number of inhibitory synapses onto compartment.
+        Inhibition results in  Chloride conductance change (Simulating GABA-R activation)
+
+        Excitatory_synapses = number of excitatory synapses onto compartment.
+        Excitation results in  Sodium conductance change (Simulating NMDA-R activation)
+
+        """
+        self.syn_t_on = syn_t_on
+        self.syn_t_off = syn_t_off
+
+        if Inhibitory_synapses != 0:
+            self.g_cl = self.g_cl * 10 * Inhibitory_synapses
+        if Excitatory_synapses != 0:
+            self.g_na = self.g_na * 10 * Excitatory_synapses
+
+        return
+
